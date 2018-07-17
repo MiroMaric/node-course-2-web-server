5d5e98d44580625d0543ce65b2fdcb814d1f1ee6
MiroMaric <miromaric1996@gmail.com>
2018-07-18 00:07:00 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
 
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
@@ -42,4 +43,17 @@ app.get('/about',(req,res)=>{
     });
 });
 
-app.listen(3000);
+app.listen(port,()=>{
+    console.log(`Server is up to port ${port}`);
+});
+
+
+/*
+git init
+git status
+git add ...
+.gitignore fajl
+git commit -m "..."
+
+SSH protokol (Secure Shell)
+*/
